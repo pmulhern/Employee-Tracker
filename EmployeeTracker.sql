f0d9e9d75f114c747f8ab9576e6e302de7128f9d
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-08-06 17:55:03 -0400

    Initial Push

---
+++ EmployeeTracker.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+id int auto_increment primary key,
+name VARCHAR(30)
+);
+
+CREATE TABLE role (
+id int auto_increment primary key,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT references department(id)
+);
+
+CREATE TABLE employee (
+id int auto_increment primary key,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id int references role(id),
+manager_id INT 
+);
+
+
+
+
+  
